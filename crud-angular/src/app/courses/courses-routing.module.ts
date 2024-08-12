1ce6bd596a6b9186014af06149adc8625ac57226
Rafa-Hoff <rafahoff32@gmail.com>
2024-08-12 16:10:35 -0400

    Criado componentes de formulario e roteamento para criar cursos

--- crud-angular/src/app/courses/courses-routing.module.ts
+++ crud-angular/src/app/courses/courses-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CoursesComponent } from './courses/courses.component';
+import { CourseFormComponent } from './course-form/course-form.component';
 
 //Configuração das rotas
 const routes: Routes = [
-  { path: '', component: CoursesComponent } //Essa rota serve para mostrar a pagina de cursos
+  { path: '', component: CoursesComponent }, //Essa rota serve para mostrar a pagina de cursos
+  { path: 'new', component: CourseFormComponent }
 ];
 
 @NgModule({
